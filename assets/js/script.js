f6e9f346fc5641357c37c4882f6076a5626533fb
dvxi <sim28888@gmail.com>
2022-08-10 16:54:11 +0200

    transformed into a single page, minor changes, more animations

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,18 @@
 AOS.init();
+
+window.onscroll = () => {
+  var current = "";
+
+  sections.forEach((section) => {
+    const sectionTop = section.offsetTop;
+    if (pageYOffset >= sectionTop ) {
+      current = section.getAttribute("id"); }
+  });
+
+  navLi.forEach((li) => {
+    li.classList.remove("active");
+    if (li.classList.contains(current)) {
+      li.classList.add("active");
+    }
+  });
+};
